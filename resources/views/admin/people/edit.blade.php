84ed9c176b44d4d4c3ee2e3d9722d254ea772e7f
magnokf <mkfelipe.rj@gmail.com>
2021-05-10 21:26:22 -0300

    #fix edit

--- resources/views/admin/people/edit.blade.php
+++ resources/views/admin/people/edit.blade.php
@@ -159,9 +159,6 @@
                             </div>
 
 
-
-
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
